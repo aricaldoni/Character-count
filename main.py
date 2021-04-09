ef48b7606f58bd01e8a0a5c1a5daac4fed86f56e
Agus <>
2021-04-09 16:48:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to the authomatic name counter")
+name = len(input("What is your name?: "))
+# We check the data type
+print(type(name))
+# We asign a string var
+new_name = str(name)
+# asign as a string
+new_name = str(name)
+print("Your name has " + new_name + " characters.")
+
